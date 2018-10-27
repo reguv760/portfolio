c6a86dde65e66afca62566f3e6f3d888f5435652
reguv760 <reguv760@gmail.com>
2018-10-26 20:16:12 -0700

    readme.md + gatsby.config info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Reggie Galang portfolio site',
   },
   pathPrefix: "/portfolio",
   plugins: [
@@ -24,7 +24,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/img/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/icons/rg-logo.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
