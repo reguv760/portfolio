03a0031a412ce10ec4ca1b7ccdd817adaa954f1d
Reginald Galang <reguv760@gmail.com>
2018-11-07 17:27:39 -0800

    adjust weblink css

--- src/components/WebLink.js
+++ src/components/WebLink.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
-//import { Link } from 'gatsby'
+// import { Link } from 'gatsby'
 
 const WebLinkStyle = styled.span`
 	cursor: pointer;
 	position: relative;
-	top: 0.75rem;
+	display:inline-block;
 `
 
 const WebLink = ({ link }) => (
-	<div>
-		<WebLinkStyle className="portfolio__link">
-			<a href={link} target="blank" rel="noopener noreferrer">
+  <div>
+    <WebLinkStyle className="portfolio__link">
+      <a href={link} target="blank" rel="noopener noreferrer">
 				Visit Website =>
-			</a>
-		</WebLinkStyle>
-	</div>
+      </a>
+    </WebLinkStyle>
+  </div>
 )
 
 export default WebLink
