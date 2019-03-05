01d8c10f864988fd82721db933bf7df27f98955f
Reginald Galang <reguv760@gmail.com>
2019-03-04 22:17:33 -0800

    gsap added + implemented to sideBarNav

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,97 +1,125 @@
 import React from 'react'
+import { TweenMax, Elastic } from 'gsap';
 import { Link } from 'gatsby'
 
 import RGlogo from './../icons/rg-logo.png'
 import LinkedIn from './../icons/linkedIn.png'
 import GitHub from './../icons/github.png'
 
+//import PortAnimation from './portfolioAnim'
+
 import Resume from './../ReginaldGalang-resume.pdf'
 
 
-const Sidebar = () => (
-  <aside className="sidebar">
-    <div className="sidebar__logo">
-      <img src={RGlogo} alt="RG:Reggie Galang glyph" />
-    </div>
+class Sidebar extends React.Component
+{
+  constructor(props){
+    super(props);
+    
+    // reference to the DOM node
+    this.myElement = null;
+    // reference to the animation
+    this.sideBarTween = null;
+  }
 
-    <nav>
-      <ul className="sidebar__nav">
-        <li>
-          <Link to="/" activeStyle={{color: '#33cf33'}}>
-            Web
-          </Link>
-        </li>
+  componentDidMount(){
+  // use the node ref to create the animation
+    if (this.myElement.getClientRects() != 0)
+      TweenLite.to(this.myElement, 0.6, {delay:0.25, left:0, ease:Bounce.easeOut});
+  }
 
-        <li>
-          <Link to="/graphic" activeStyle={{color: '#33cf33'}}>
-            Graphic Design
-          </Link>
-        </li>
 
-        <li>
-          <Link to="/about" activeStyle={{color: '#33cf33'}}>
-            About Me
-          </Link>
-        </li>
-        <li>
-          <a href={Resume} target="_blank" rel="noopener noreferrer">
-            R&eacute;sum&eacute;
-          </a>
-        </li>
-
-      </ul>
-    </nav>
-
-    <div className="sidebar__header">
-      <p>
-        Hello, my name is Reginald Galang and I'm a Web Developer + Graphic
-        Designer. Feel free to browse through my projects.
-      </p>
-      <p>
-        <a href="mailto:reguv760@gmail.com">reguv760@gmail.com</a>
-      </p>
-    </div>
-
-    <div className="sidebar__footer">
-      <ul className="socialmedia">
-        <li>
-          <a
-            href="https://www.linkedin.com/in/reggiegalang/"
-            target="_blank"
-            className="svg"
-            rel="noopener noreferrer"
-          >
-            <img src={LinkedIn} alt="Linkedin Icon" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://github.com/reguv760/"
-            target="_blank"
-            className="svg"
-            rel="noopener noreferrer"
-          >
-            <img src={GitHub} alt="Github Icon" />
-          </a>
-        </li>
-      </ul>
-
-      <div className="sidebar__footerText">
-        <p>#using CSS Grid + Flexbox</p>
-        <p>
-          Built with{' '}
-          <a
-            href="http://gatsbyjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GatsbyJS
-          </a>
-        </p>
-        <p>Site updated: 1.28.19</p>
+  render()
+  {
+    return (
+     <aside className="sidebar" >
+      <div className="sidebar__logo" >
+          <img src={RGlogo} alt="RG:Reggie Galang glyph" />
       </div>
-    </div>
-  </aside>
-)
+
+      <nav >
+        { /*ref={div => this.myElement = div}} is to call TweenLite
+          in componentDidMount() */ }
+        <ul className="sidebar__nav" ref={div => this.myElement = div}>
+          <li>
+            <Link to="/" activeStyle={{color: '#33cf33'}}>
+              Web
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/graphic" activeStyle={{color: '#33cf33'}}>
+              Graphic Design
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/about" activeStyle={{color: '#33cf33'}}>
+              About Me
+            </Link>
+          </li>
+          <li>
+            <a href={Resume} target="_blank" rel="noopener noreferrer">
+              R&eacute;sum&eacute;
+            </a>
+          </li>
+
+        </ul>
+      </nav>
+
+      <div className="sidebar__header">
+        <p>
+          Hello, my name is Reginald Galang and I'm a Web Developer + Graphic
+          Designer. Feel free to browse through my projects.
+        </p>
+        <p>
+          <a href="mailto:reguv760@gmail.com">reguv760@gmail.com</a>
+        </p>
+      </div>
+
+      <div className="sidebar__footer">
+        <ul className="socialmedia">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/reggiegalang/"
+              target="_blank"
+              className="svg"
+              rel="noopener noreferrer"
+            >
+              <img src={LinkedIn} alt="Linkedin Icon" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/reguv760/"
+              target="_blank"
+              className="svg"
+              rel="noopener noreferrer"
+            >
+              <img src={GitHub} alt="Github Icon" />
+            </a>
+          </li>
+        </ul>
+
+        <div className="sidebar__footerText">
+          <p>#using CSS Grid + Flexbox</p>
+          <p>
+            Built with{' '}
+            <a
+              href="http://gatsbyjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GatsbyJS
+            </a>
+          </p>
+          <p>Site updated: 1.28.19</p>
+        </div>
+      </div>
+    </aside>
+    )
+  }
+ 
+}
 
 export default Sidebar
--- src/components/portfolioAnim.js
+++ src/components/portfolioAnim.js
@@ -1,26 +1,67 @@
-import { TweenMax, Elastic } from 'gsap'
+import { component } from 'react';
+import { TweenMax, Elastic } from 'gsap';
 
-const duration = 0.5
+//const duration = 0.5
+class PortAnimation extends Component 
+{
+	//init animation by "inheriting" component lib
+	constructor(props){
+		super(props);
+		
+		// reference to the DOM node
+		this.myElement = null;
+		// reference to the animation
+		this.sideBarTween = null;
+	}
 
-export default {
-	show(target, cb) {
-		return TweenMax.from(target, duration, {
-			opacity: 0,
-			height: 0,
-			onComplete() {
-				cb()
-			},
-			ease: Elastic.easeOut.config(0.25, 1),
-		})
-	},
-	hide(target, cb) {
-		return TweenMax.to(target, duration, {
-			opacity: 0,
-			height: 0,
-			onComplete() {
-				cb()
-			},
-			ease: Elastic.easeIn.config(0.25, 1),
-		})
-	},
+	componentDidMount(){
+	// use the node ref to create the animation
+		TweenLite.to(this.myElement, 0.6, {delay:0.25, left:0, ease:Bounce.easeOut});
+	}
+
+	render(){
+		return <div ref={div => this.myElement = div} />;
+	}
 }
+
+export default PortAnimation;
+
+//animation for sideName
+//sideNav.style.left = "-15rem";
+//TweenMax.to(sideNav, 0.6, {delay:0.25,left:0, ease:Bounce.easeOut});
+
+
+
+//portfolio overlay animation:::
+//   for (var i = 0; i < portOverlay.length; i++ )
+//   {
+//     //console.log(i);
+//     portOverlay[i].style.opacity = "0";
+
+//     portOverlay[i].addEventListener("mouseenter", function( event ) 
+//     {     
+//       var textContainer = this.getElementsByClassName("portfolio__overlay-container")[0];
+
+//       TweenMax.killTweensOf(event.target);
+//       TweenMax.killTweensOf(textContainer);
+
+//       textContainer.style.top = "20%";
+//       textContainer.style.transform = "scaleX(0)";
+
+      
+//       TweenMax.to(event.target, 0.25, {opacity:1, ease:Power2.easeOut});
+//       TweenMax.to(textContainer, 0.3, {scaleX:1, top:0, delay:0.2, transformOrigin:"50% 50%", ease:Circ.easeOut});
+
+//     });
+
+//     portOverlay[i].addEventListener("mouseleave", function( event ) 
+//     {
+//       var textContainer = this.getElementsByClassName("portfolio__overlay-container")[0];
+
+//       TweenMax.killTweensOf(event.target);
+//       TweenMax.killTweensOf(textContainer);
+
+//       TweenMax.to(event.target, 0.25, {opacity:0, ease:Power1.easeOut});
+//       TweenMax.to(textContainer, 0.25, {scaleX:0, ease:Power1.easeOut});
+//     });   
+// }
