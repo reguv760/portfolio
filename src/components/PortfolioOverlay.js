9a44cb6f783f7b0594bbc5d18112f940cdc09a72
Reginald Galang <reguv760@gmail.com>
2019-03-19 20:21:42 -0700

    PortfolioOverlay fix

--- src/components/PortfolioOverlay.js
+++ src/components/PortfolioOverlay.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Tween } from 'react-gsap'
 
-
-const initOverlayStyle =
+const initTextStyle =
 {
 	opacity: `0`,
+	marginTop: `2rem`,
 }
 
 class PortfolioOverlay extends React.Component
@@ -16,25 +16,28 @@ class PortfolioOverlay extends React.Component
 
 	mouseEnter = () => {
 		console.log("enter");
-		//this.setState({ isMouseInside: true });
 	}
 
 	mouseLeave = () => {
-		//this.setState({ isMouseInside: false });
 		console.log("out");
 	}
 
 	render()
 	{
+
+		{ /* <div className="portfolio__overlay"  onMouseEnter={ this.mouseEnter } onMouseLeave={ this.mouseLeave } > */ }
 	  return (    
-		<div className="portfolio__overlay">
-      		<Tween duration={ '0.5' } to={{ delay:`0.5`, opacity:`1`, ease:`Power2.easeOut` }} >
-				<div className="portfolio__overlay-container" style={initOverlayStyle} onMouseEnter={ this.mouseEnter } onMouseLeave={ this.mouseLeave }>
-				  <p className="portfolio__overlay-text">
+		<div className="portfolio__overlay" >
+			<div className="portfolio__overlay-container">
+				<Tween 
+					duration={ '0.5' } 
+					to={{ delay:`0.5`, marginTop:`0`, opacity:`1`, ease:`Power2.easeOut` }} 
+				>
+				  <p className="portfolio__overlay-text" style={ initTextStyle }>
 					{ this.props.children }
 				  </p>
-				</div>
-			</Tween>
+				</Tween>
+			</div>
 		</div>
 	  )
 	}
