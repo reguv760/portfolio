260c7dcf42c53601569dc1a75b7509dde3cc260a
Reginald Galang <reguv760@gmail.com>
2019-05-02 12:48:27 -0700

    add siteUpdateDate to graphic page

--- src/pages/graphic.js
+++ src/pages/graphic.js
@@ -1,53 +1,54 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import styled from 'styled-components'
-import { StaticQuery, graphql } from 'gatsby'
+import React from "react";
+import Helmet from "react-helmet";
+import styled from "styled-components";
+import { StaticQuery, graphql } from "gatsby";
 
-import Layout from './../components/layout'
-import Sidebar from './../components/Sidebar'
-import MobileNav from './../components/MobileNav'
-import PageContainer from './../components/PageContainer'
-import ContainerHeader from './../components/ContainerHeader'
+import Layout from "./../components/layout";
+import Sidebar from "./../components/Sidebar";
+import MobileNav from "./../components/MobileNav";
+import PageContainer from "./../components/PageContainer";
+import ContainerHeader from "./../components/ContainerHeader";
 
 // page structure:::
-import Pagination from './../components/Pagination'
-import PageFooter from './../components/PageFooter'
+import Pagination from "./../components/Pagination";
+import PageFooter from "./../components/PageFooter";
 
 // projects
-import RavenwolfLogo from './../projects/Ravenwolf'
-import SetaLogo from './../projects/Seta'
+import RavenwolfLogo from "./../projects/Ravenwolf";
+import SetaLogo from "./../projects/Seta";
 
 // styled component for page structure
 // used once
 const MainSection = styled.section`
-	width: 100%;
-	background-color: #efefef;
-`
+  width: 100%;
+  background-color: #efefef;
+`;
 
 // this is the structure for /graphic
 const GraphicPage = () => (
   <StaticQuery
     query={graphql`
-			query gdTitleQuery {
-				site {
-					siteMetadata {
-						title
-					}
-				}
-			}
-		`}
+      query gdTitleQuery {
+        site {
+          siteMetadata {
+            title
+            siteUpdateDate
+          }
+        }
+      }
+    `}
     render={data => (
       <Layout>
-        <Helmet title={data.site.siteMetadata.title + ' : graphic design'} />
+        <Helmet title={data.site.siteMetadata.title + " : graphic design"} />
         <div className="mainContainer">
-          <Sidebar />
+          <Sidebar siteUpdateDate={data.site.siteMetadata.siteUpdateDate} />
           <MobileNav />
           <PageContainer name="container">
             <ContainerHeader />
 
-           { /* <MainSection>
+            {/* <MainSection>
               <Pagination />
-            </MainSection> */ }
+            </MainSection> */}
 
             {/* below is the meat of the structure */}
             <MainSection>
@@ -55,18 +56,20 @@ const GraphicPage = () => (
               <SetaLogo />
             </MainSection>
 
-            { /* <MainSection>
+            {/* <MainSection>
               <Pagination />
-            </MainSection> */ }
-            
+            </MainSection> */}
+
             {/* only visible < Tablet res */}
-            <PageFooter />
+            <PageFooter
+              siteUpdateDate={data.site.siteMetadata.siteUpdateDate}
+            />
           </PageContainer>
           {/*  <Link to="/page-2/">Go to page 2</Link> */}
         </div>
       </Layout>
     )}
   />
-)
+);
 
-export default GraphicPage
+export default GraphicPage;
